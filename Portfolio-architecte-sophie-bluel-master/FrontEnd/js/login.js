dab4c047a2a54335c03eddc0730af1a6218daf2c
Nataormina <n.taormina@hotmail.com>
2024-08-13 14:16:30 +0200

    Page de login et token

---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/login.js
@@ -0,0 +1,161 @@
+/*// Déclaration des variables du formulaire de connexion et ses inputs
+const loginForm = document.querySelector("#login form");
+const inputsForm = document.querySelectorAll("#login input");
+
+// Fonction pour indiquer une erreur
+const errorDisplay = (tag, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const label = document.querySelector("." + tag + "-container > label");
+
+  if (!valid) {
+    container.classList.add("error");
+    label.classList.add("error");
+  } else {
+    container.classList.remove("error");
+    label.classList.remove("error");
+  }
+};
+
+// Fonction pour afficher le message d'erreur
+const errorConnection = (valid) => {
+  const pError = document.querySelector(".erreur-connexion");
+
+  if (!valid) {
+    pError.classList.add("error");
+  } else {
+    pError.classList.remove("error");
+  }
+};
+
+// Fonction pour vérifier que l'email fonctionne bien à l'input
+const emailChecker = (value) => {
+  if (value.length == 0) {
+    errorDisplay("email");
+  } else if (!value.match(/^\w+[\w.-]*@\w+(-\w+)*\.\w{2,4}$/)) {
+    errorDisplay("email");
+  } else if (!value === "sophie.bluel@test.tld") {
+    errorDisplay("email");
+  } else {
+    errorDisplay("email", true);
+  }
+};
+
+// Fonction pour vérifier que le mot de passe fonctionne bien à l'input
+const passwordChecker = (value) => {
+  if (value.length == 0) {
+    errorDisplay("password");
+  } else {
+    errorDisplay("password", true);
+  }
+};
+
+inputsForm.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "emailLog":
+        emailChecker(e.target.value);
+        break;
+      case "passwordForget":
+        passwordChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
+
+// eventListener submit au clique sur le bouton pour se connecter après avoir rempli le formulaire
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Récupération des valeurs des inputs en objet
+  const completedForm = {
+    email: emailLog.value,
+    password: passwordForget.value,
+  };
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(completedForm),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("userId", data.userId);
+        window.location.href = "index.html";
+      } else {
+        errorDisplay("email");
+        errorDisplay("mdp");
+        errorConnection();
+      }
+    });
+});*/
+
+
+
+const loginApi = "http://localhost:5678/api/users/login";
+
+
+document.getElementById("login").addEventListener("submit", pushSubmit);
+
+
+
+async function pushSubmit(event) {
+  event.preventDefault();
+
+  let user = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+  
+  let response = await fetch(loginApi, {
+   method: "POST",
+   headers: {
+    "Content-Type": "application/json",
+   },
+   body: JSON.stringify(user),
+    });
+  console.log(response);
+  if (response.status != 200) {
+    const errorMessageLogin = document.createElement("div");
+    errorMessageLogin.className = "error-login";
+    errorMessageLogin.innerHTML = "Erreur de connexion, veuillez vérifier votre identifiant et mot de passe";
+    document.querySelector("form").prepend(errorMessageLogin);
+   
+    
+  }
+  
+    let result = await response.json();
+    const token = result.token;
+    sessionStorage.setItem("authToken", token);
+    console.log(token);
+  }
+
+
+
+// Aide fournie par Companion :
+/*function getInputs() {
+  // Récupérer les valeurs des champs d'e-mail et de mot de passe
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Vous pouvez maintenant utiliser les valeurs des champs d'e-mail et de mot de passe comme vous le souhaitez
+  console.log('E-mail:', email);
+  console.log('Mot de passe:', password);
+}
+getInputs();
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Récupérer les valeurs des champs d'e-mail et de mot de passe
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  console.log('E-mail:', email);
+  console.log('Mot de passe:', password);
+});*/
+  
--- Portfolio-architecte-sophie-bluel-master/FrontEnd/js/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/script.js
@@ -1,65 +1,123 @@
 
 
-/*Variables*/
+ // Récupération des projets avec l'api
+fetch("http://localhost:5678/api/works")
+.then((response) => response.json())
+.then((data) => {
+  // Ajout des projets dans la div gallery
+  const gallery = document.querySelector(".gallery");
 
+  // Création d'une boucle for pour éviter de répéter l'opération pour les 11 projets
+  const createGalleryMain = () => {
+    for (let i = 0; i < data.length; i++) {
+      const figure = document.createElement("figure");
+      figure.id = `mainFigure-${data[i].id}`;
+      figure.innerHTML = `<img src="${data[i].imageUrl}" alt="${data[i].title}" data-type="${data[i].category.name}" data-id="${data[i].id}">
+      <figcaption>${data[i].title}</figcaption>`;
+      gallery.appendChild(figure);
+    }
+  };
+  createGalleryMain();
 
+  // Déclaration de la variable portfolio pour y ajouter les filtres
+  const portfolio = document.getElementById("portfolio");
 
-/*Fonction qui récupère les travaux et les affichent dans la console*/
-/*async function getWorks() {
-    const response = await fetch ("http://localhost:5678/api/works");
-    const responseJson = await response.json()
-    console.log(responseJson);*/
-    
+  // Création d'un objet set pour récupérer les filtres correspondant aux projets sans les doublons
+  const categories = new Set();
 
-    async function getWorks() {
-      const url = "http://localhost:5678/api/works";
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
+  categories.add("Tous");
+  data.forEach((item) => {
+    categories.add(item.category.name);
+  });
+
+  // "Transformer" l'objet set en tableau afin d'exploiter ses données
+  const categoriesArray = [...categories];
+
+  // Ajout des boutons pour filtrer les projets dans la section portfolio
+  const filtres = document.createElement("div");
+  filtres.classList.add("btn-filter");
+
+  categoriesArray.forEach((cate) => {
+    const btnFiltre = document.createElement("button");
+    btnFiltre.textContent = cate;
+    btnFiltre.classList.add("btn");
+    filtres.appendChild(btnFiltre);
+  });
+
+  portfolio.appendChild(filtres);
+  portfolio.insertBefore(filtres, gallery);
+
+  // Création de l'événement au clique sur les boutons filtres pour trier les images
+  const figures = document.querySelectorAll(".gallery figure");
+  const btnsFiltres = document.querySelectorAll(".btn");
+
+  btnsFiltres.forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
+      const btnClicked = e.target;
+      const nameFilter = btnClicked.textContent;
+
+      btnsFiltres.forEach((btn) => {
+        if (btn === btnClicked) {
+          btn.classList.add("active-btn");
+        } else {
+          btn.classList.remove("active-btn");
         }
-      
-        const json = await response.json();
-        console.log(json);
-        for (let i = 0; i < json.length; i++) {
-        setFigure(json[i]);
+      });
+
+      figures.forEach((figure) => {
+        const image = figure.querySelector("img");
+        const imageType = image.dataset.type;
+
+        if (nameFilter === "Tous" || nameFilter === imageType) {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+      });
+    });
+
+    if (index === 0) {
+      btn.classList.add("active-btn");
     }
-    } catch (error) {
-    console.error(error.message);
-   }
-  }
-  getWorks();
+  });
 
-/*Ajout des travaux au DOM */
-function setFigure(data) {
-const figure = document.createElement("figure");
-figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
-				<figcaption>${data.title}</figcaption>`;
+  // Fonction pour supprimer les filtres quand nous sommes connectés
+  const deleteFiltres = () => {
+    if (localStorage.getItem("userId") && localStorage.getItem("token")) {
+      filtres.style.display = "none";
+    }
+  };
+  deleteFiltres();
+});
 
-document.querySelector(".gallery").append(figure);
+/*// eventListener pour accéder à la page de connexion ou se déconnecter
+const loginPage = document.getElementById("login");
+
+loginPage.addEventListener("click", () => {
+if (localStorage.getItem("userId") && localStorage.getItem("token")) {
+  // Si connecté, effectuer la déconnexion et maj du texte du lien
+  localStorage.clear();
+  loginLi.textContent = "login";
+  window.location.href = "index.html";
+} else {
+  window.location.href = "login.html";
 }
+});
+
+// Si connecté, effectuer toutes les modifs pour être sur le mode édition
+document.addEventListener("DOMContentLoaded", () => {
+if (localStorage.getItem("userId") && localStorage.getItem("token")) {
+  loginPage.textContent = "logout";
+
+  const modifModeBar = document.querySelector(".publish-changes");
+  modifModeBar.style.display = "flex";
+
+  const header = document.querySelector("header");
+  header.style.paddingTop = "40px";
+
+  const modifSpan = document.querySelector(".span-modif");
+  modifSpan.classList.add("display-modif");
+}
+});*/
+
  
- /* Ajout Catégories */
- async function getCategories() {
-  const url = "http://localhost:5678/api/categories";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    for (let i = 0; i < json.length; i++) {
-      setFilter(json[i]);
-    }
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-getCategories();
-
-function setFilter(data) {
-  const div = document.createElement("div");
-  div.innerHTML = `${data.name}`;
-  document.querySelector(".btn-filter").append(div)
-}
