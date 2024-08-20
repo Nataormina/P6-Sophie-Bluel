57c24889e77f717dff4d0c48ba6720e9ca1857ca
Nataormina <n.taormina@hotmail.com>
2024-08-20 20:42:35 +0200

    Suppression des filtres en Mode Administrateur

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/js/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/script.js
@@ -1,39 +1,33 @@
 
-
- // Récupération des projets avec l'api
+ // Récupération des projets avec l'API
 fetch("http://localhost:5678/api/works")
 .then((response) => response.json())
 .then((data) => {
   // Ajout des projets dans la div gallery
   const gallery = document.querySelector(".gallery");
 
-  // Création d'une boucle for pour éviter de répéter l'opération pour les 11 projets
+  // Création d'une boucle pour ajouter tous les projets
   const createGalleryMain = () => {
     for (let i = 0; i < data.length; i++) {
       const figure = document.createElement("figure");
       figure.id = `mainFigure-${data[i].id}`;
-      figure.innerHTML = `<img src="${data[i].imageUrl}" alt="${data[i].title}" data-type="${data[i].category.name}" data-id="${data[i].id}">
-      <figcaption>${data[i].title}</figcaption>`;
+      figure.innerHTML = `
+        <img src="${data[i].imageUrl}" alt="${data[i].title}" data-type="${data[i].category.name}" data-id="${data[i].id}">
+        <figcaption>${data[i].title}</figcaption>`;
       gallery.appendChild(figure);
     }
   };
   createGalleryMain();
 
-  // Déclaration de la variable portfolio pour y ajouter les filtres
+  // Création des filtres
   const portfolio = document.getElementById("portfolio");
-
-  // Création d'un objet set pour récupérer les filtres correspondant aux projets sans les doublons
   const categories = new Set();
-
   categories.add("Tous");
   data.forEach((item) => {
     categories.add(item.category.name);
   });
 
-  // "Transformer" l'objet set en tableau afin d'exploiter ses données
   const categoriesArray = [...categories];
-
-  // Ajout des boutons pour filtrer les projets dans la section portfolio
   const filtres = document.createElement("div");
   filtres.classList.add("btn-filter");
 
@@ -44,10 +38,9 @@ fetch("http://localhost:5678/api/works")
     filtres.appendChild(btnFiltre);
   });
 
-  portfolio.appendChild(filtres);
   portfolio.insertBefore(filtres, gallery);
 
-  // Création de l'événement au clique sur les boutons filtres pour trier les images
+  // Ajouter l'événement de clic sur les filtres
   const figures = document.querySelectorAll(".gallery figure");
   const btnsFiltres = document.querySelectorAll(".btn");
 
@@ -81,133 +74,53 @@ fetch("http://localhost:5678/api/works")
     }
   });
 
-  // Fonction pour supprimer les filtres quand nous sommes connectés
-  const deleteFiltres = () => {
-    if (localStorage.getItem("userId") && localStorage.getItem("token")) {
-      filtres.style.display = "none";
-    }
-  };
-  deleteFiltres();
-});
-
-/*// eventListener pour accéder à la page de connexion ou se déconnecter
-const loginPage = document.getElementById("login");
-
-loginPage.addEventListener("click", () => {
-if (localStorage.getItem("userId") && localStorage.getItem("token")) {
-  // Si connecté, effectuer la déconnexion et maj du texte du lien
-  localStorage.clear();
-  loginLi.textContent = "login";
-  window.location.href = "index.html";
-} else {
-  window.location.href = "login.html";
-}
-});
-
-// Si connecté, effectuer toutes les modifs pour être sur le mode édition
-document.addEventListener("DOMContentLoaded", () => {
-if (localStorage.getItem("userId") && localStorage.getItem("token")) {
-  loginPage.textContent = "logout";
-
-  const modifModeBar = document.querySelector(".publish-changes");
-  modifModeBar.style.display = "flex";
-
-  const header = document.querySelector("header");
-  header.style.paddingTop = "40px";
-
-  const modifSpan = document.querySelector(".span-modif");
-  modifSpan.classList.add("display-modif");
-}
-});*/
-
-
-
-
-
-
-// Affichage de la modale au clic sur Mode édition
-let bannerEdition;// Déclarer la variable en dehors de la fonction
-
-
-
-
-function displayModeAdmin () {
-  if (sessionStorage.authToken) {
-    console.log("ok");
-    bannerEdition = document.createElement("div"); // Supprimer le "const"
-    bannerEdition.className = "modeEdition";
-    bannerEdition.innerHTML = '<p><a href=""><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>';
-    loginLi.innerHTML = '<a href="#">logout</a>';
-    document.getElementById("loginLi").addEventListener("click", function(event) {
-      event.preventDefault(); // Empêche le comportement par défaut de l'ancre  
-      sessionStorage.removeItem('authToken'); // Supprime le token de session  
-      // Vous pouvez également rediriger l'utilisateur ou mettre à jour l'interface ici  
-      console.log("Déconnecté");
-    });
-    
-    document.body.prepend(bannerEdition);
-    
+  // Vérifier si l'utilisateur est connecté et cacher les filtres en mode édition
+  if (sessionStorage.getItem("authToken")) {
+    filtres.style.display = "none";
   }
-}
+});
 
+// Gérer le mode administrateur
+function displayModeAdmin() {
+if (sessionStorage.authToken) {
+  console.log("ok");
+  bannerEdition = document.createElement("div"); // Créer la bannière
+  bannerEdition.className = "modeEdition";
+  bannerEdition.innerHTML = '<p><a href=""><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>';
+  loginLi.innerHTML = '<a href="#">logout</a>';
+
+  document.getElementById("loginLi").addEventListener("click", function(event) {
+    event.preventDefault();
+    sessionStorage.removeItem('authToken');
+    console.log("Déconnecté");
+    window.location.reload();
+  });
+
+  document.body.prepend(bannerEdition);
+}
+}
 
 const containerModals = document.querySelector(".containerModals");
 const loginLi = document.getElementById('loginLi');
 
-
 displayModeAdmin();
 
-
-
-
-
-// Vérifier si bannerEdition existe avant d'ajouter l'écouteur d'événement
 if (bannerEdition) {
-  // Affichage de la modale au clic sur Mode édition
-  bannerEdition.addEventListener("click", (event) => {
-    console.log("modeEdition");
-    event.preventDefault(); 
-    containerModals.style.display = "flex";  
-    loginLi.innerHTML = '<a href="#">logout</a>';
-    
+bannerEdition.addEventListener("click", (event) => {
+  event.preventDefault();
+  containerModals.style.display = "flex";
 });
 }
 
-
-
-
-
-
- 
-    
-
-
-// Ferme la modale lorsque l'utilisateur clique en dehors de la modale
 window.onclick = function(event) {
-  if (event.target == containerModals) {
-    containerModals.style.display = "none";
-  }
+if (event.target == containerModals) {
+  containerModals.style.display = "none";
+}
 }
 
-
- // Ferme la modale lorsque l'utilisateur clique sur (x)
 document.addEventListener("DOMContentLoaded", function() {
-  var span = document.querySelector(".close");
-  var containerModals = document.querySelector(".containerModals");
-
-  span.addEventListener("click", function() {
-    containerModals.style.display = "none";
-  });
+var span = document.querySelector(".close");
+span.addEventListener("click", function() {
+  containerModals.style.display = "none";
 });
-
-
-
-
-
-
-   
-
-
-
-
-
+});
