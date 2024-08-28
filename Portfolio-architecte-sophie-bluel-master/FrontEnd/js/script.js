5c548d8a10bcb9eb7ec5bd3274a84aa88ccc0355
Nataormina <n.taormina@hotmail.com>
2024-08-28 12:11:52 +0200

    Mise en forme 2ème modale ajout photo

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/js/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/script.js
@@ -234,69 +234,7 @@ window.onclick = function(event) {
   });
 
 
-/* // Créer l'input file caché pour sélectionner les photos
-  const photoInput = document.createElement("input");
-  photoInput.type = "file";
-  photoInput.accept = "image/*";
-  photoInput.multiple = true;
-  photoInput.style.display = "none";
 
-  // Ajouter l'input au body pour pouvoir y accéder via le JavaScript
-  document.body.appendChild(photoInput);
-
-  // Ajouter l'écouteur d'événement pour le bouton "Ajouter une photo"
-  addPhotoButton.addEventListener("click", () => {
-    photoInput.click(); // Ouvre la boîte de dialogue pour sélectionner des fichiers
-  });
-
-  // Gérer l'ajout des photos après la sélection
-  photoInput.addEventListener("change", (event) => {
-    const files = event.target.files;
-    if (files.length > 0) {
-      // Parcourir les fichiers sélectionnés et les afficher dans la modale
-      for (const file of files) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-          const img = document.createElement("img");
-          img.src = e.target.result;
-          img.style.maxWidth = "100px";
-          img.style.margin = "10px";
-          projetModal.appendChild(img);
-        };
-        reader.readAsDataURL(file);
-  
-    }
-  }
-  });
-
-   
-  // Chemin de base vers le dossier d'images
-  const basePath = 'images/';
- 
-
-  // Charger les images et les afficher dans la modale
-  images.forEach((imageName) => {
-    const img = document.createElement("img");
-    img.src = `${basePath}${imageName}`;
-    img.alt = imageName;
-    img.style.maxWidth = "100px";
-    img.style.margin = "10px";
-
-const deleteIcon = document.createElement("img");
-    deleteIcon.src = "path/to/trash-icon.png"; // Chemin de l'icône de poubelle
-    deleteIcon.className = "delete-icon";
-
-    // Ajouter un événement pour supprimer l'image
-    deleteIcon.addEventListener("click", () => {
-      projetModal.removeChild(img);
-    });
-
-    const imageContainer = document.createElement("div");
-    imageContainer.appendChild(img);
-    imageContainer.appendChild(deleteIcon);
-
-    projetModal.appendChild(img);
-  });*/
 
 
 
